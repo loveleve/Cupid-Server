de6bf634c0edc1c94577baeef44982cb45e4cda4
nogyeongtae <nokt@crowdworks.kr>
2023-01-09 22:40:55 +0900

    git init

---
+++ src/test/java/team/arrow/cupid/CupidApplicationTests.java
@@ -0,0 +1,13 @@
+package team.arrow.cupid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CupidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
